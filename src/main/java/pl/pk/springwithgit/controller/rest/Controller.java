209e615da85489a422df5af028380878a73f59f9
PatrykKoc <patryk.koc44@gmail.com>
2019-12-15 11:35:18 +0100

    [add] - rest controller branch

---
+++ src/main/java/pl/pk/springwithgit/controller/rest/Controller.java
@@ -0,0 +1,9 @@
+package pl.pk.springwithgit.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@org.springframework.stereotype.Controller
+@RestController
+public class Controller {
+
+}
