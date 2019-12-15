badaf1148381e0e627eaa7932a377da67bae2fd0
PatrykKoc <patryk.koc44@gmail.com>
2019-12-15 09:52:43 +0100

    [init] - project skeleton

---
+++ src/main/java/pl/pk/springwithgit/SpringWithGitApplication.java
@@ -0,0 +1,13 @@
+package pl.pk.springwithgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pk/springwithgit/SpringWithGitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pk.springwithgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
